08692c893264cf234419e469b246eae4595c1392
goncalopalaio <goncalo.palaio@gmail.com>
2021-12-01 12:00:31 +0000

    download input for day 1 ; placeholder files

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,13 @@
+import java.io.File
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+
+    val file = File("inputs/test.txt")
+    val content = file.bufferedReader().readLines()
+    print("Content: $content")
 }
