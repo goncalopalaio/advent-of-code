f971ea69daf3de424e7e3234be1616f25385cf87
goncalopalaio <goncalo.palaio@gmail.com>
2021-12-02 18:33:11 +0000

    day 2 solution

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -2,12 +2,56 @@ import Debug.log
 import java.io.File
 import java.lang.RuntimeException
 
-fun main(args: Array<String>) {
+fun main() {
     val inputDemo = File("inputs/2021/day_2_demo.txt").readLines()
-    measurePrint("Part 1 Demo") { part1(inputDemo) }
-
     val input = File("inputs/2021/day_2.txt").readLines()
+
+    measurePrint("Part 1 Demo") { part1(inputDemo) }
     measurePrint("Part 1") { part1(input) }
+
+    measurePrint("Part 2 Demo") { part2(inputDemo) }
+    measurePrint("Part 2") { part2(input) }
+
+    /**
+        150              | Part 1 Demo                      | 0ms
+        1488669          | Part 1                           | 1ms
+        900              | Part 2 Demo                      | 0ms
+        1176514794       | Part 2                           | 1ms
+     */
+}
+
+private fun part2(input: List<String>): Int {
+    var horizontalPosition = 0
+    var aim = 0
+    var depth = 0
+
+    for (line in input) {
+        val direction = line[0]
+        val amount: Int
+        when (direction) {
+            'f' -> {
+                amount = line.substring("forward ".length).toInt()
+                horizontalPosition += amount
+                depth += aim * amount
+            }
+            'd' -> {
+                amount = line.substring("down ".length).toInt()
+                aim += amount
+            }
+            'u' -> {
+                amount = line.substring("up ".length).toInt()
+                aim -= amount
+            }
+            else -> {
+                throw RuntimeException("Unexpected value, line=$line")
+            }
+        }
+
+        // log("direction=$direction, amount=$amount")
+    }
+
+    // log("horizontalPosition=$horizontalPosition, depth=$depth")
+    return horizontalPosition * depth
 }
 
 private fun part1(input: List<String>): Int {
@@ -16,17 +60,22 @@ private fun part1(input: List<String>): Int {
     for (line in input) {
         val direction = line[0]
         val amount: Int
-        if (direction == 'f') {
-            amount = line.substring("forward ".length).toInt()
-            horizontalPosition += amount
-        } else if(direction == 'd') {
-            amount = line.substring("down ".length).toInt()
-            depth += amount
-        } else if(direction == 'u') {
-            amount = line.substring("up ".length).toInt()
-            depth -= amount
-        } else {
-            throw RuntimeException("Unexpected value, line=$line")
+        when (direction) {
+            'f' -> {
+                amount = line.substring("forward ".length).toInt()
+                horizontalPosition += amount
+            }
+            'd' -> {
+                amount = line.substring("down ".length).toInt()
+                depth += amount
+            }
+            'u' -> {
+                amount = line.substring("up ".length).toInt()
+                depth -= amount
+            }
+            else -> {
+                throw RuntimeException("Unexpected value, line=$line")
+            }
         }
 
         // log("direction=$direction, amount=$amount")
