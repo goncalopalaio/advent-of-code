db98b3463a63b1265347093284cc443f98b45095
goncalopalaio <goncalo.palaio@gmail.com>
2021-12-13 23:52:56 +0000

    day 12 and day 13 ; moves stack to general

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -113,44 +113,6 @@ fun isOpenChar(s: String): Boolean {
     }
 }
 
-class Stack<T> {
-    private var backing = ArrayList<T>()
-
-    fun push(element: T) {
-        backing.add(element)
-    }
-
-    fun pop(): T? {
-        val result = if (backing.isEmpty()) {
-            null
-        } else {
-            val value = backing[backing.lastIndex]
-            backing.removeAt(backing.lastIndex)
-            value
-        }
-
-        return result
-    }
-
-    fun isNotEmpty() = backing.isNotEmpty()
-
-    fun clear() = backing.clear()
-
-    fun map(mapping: (T) -> T) {
-        backing = backing.map { mapping(it) } as ArrayList<T>
-    }
-
-    fun reverse() {
-        backing.reverse()
-    }
-
-    fun listIterator() = backing.iterator()
-
-    override fun toString(): String {
-        return "Stack(${backing.joinToString()})"
-    }
-}
-
 private fun parse(input: List<String>): Array<Array<String>> {
     val output = ArrayList<Array<String>>()
     for (line in input) {
--- src/main/kotlin/General.kt
+++ src/main/kotlin/General.kt
@@ -96,3 +96,42 @@ private fun <T> quickPerm(a: Array<T>): ArrayList<Array<T>> {
     return permutations
 }
 
+class Stack<T> {
+    private var backing = ArrayList<T>()
+
+    fun push(element: T) {
+        backing.add(element)
+    }
+
+    fun pop(): T? {
+        val result = if (backing.isEmpty()) {
+            null
+        } else {
+            val value = backing[backing.lastIndex]
+            backing.removeAt(backing.lastIndex)
+            value
+        }
+
+        return result
+    }
+
+    fun isNotEmpty() = backing.isNotEmpty()
+
+    fun clear() = backing.clear()
+
+    fun map(mapping: (T) -> T) {
+        backing = backing.map { mapping(it) } as ArrayList<T>
+    }
+
+    fun reverse() {
+        backing.reverse()
+    }
+
+    fun listIterator() = backing.iterator()
+
+    override fun toString(): String {
+        return "Stack(${backing.joinToString()})"
+    }
+}
+
+
