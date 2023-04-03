40a87e67e37a7bc240dacc51f30ec0e1813a2dea
goncalopalaio <goncalo.palaio@gmail.com>
2023-04-03 17:20:35 +0100

    Day 15 - Inputs

---
+++ src/main/kotlin/Day15.kt
@@ -0,0 +1,31 @@
+import harness.runProblem
+import java.io.File
+
+private fun main() = day15()
+
+fun day15() {
+    val day = "day_15"
+    val inputDemo = File("inputs/2022/${day}_demo.txt").readLines()
+    val input = File("inputs/2022/$day.txt").readLines()
+
+    runProblem("Part 1 (Demo)") { part1(inputDemo) }
+    // runProblem("Part 1") { part1(input) }
+    // runProblem("Part 2 (Demo)") { part2(inputDemo) }
+    // runProblem("Part 2") { part2(input) }
+}
+
+private fun part2(input: List<String>): Int {
+    return 0
+}
+
+private fun part1(input: List<String>): Int {
+    return 0
+}
+
+private fun parse(input: List<String>): List<String> {
+    for (line in input) {
+        println(line)
+    }
+
+    return emptyList()
+}
